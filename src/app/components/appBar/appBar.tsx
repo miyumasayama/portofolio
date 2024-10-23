565e47116287798794db933152025076408a98fe
Miyu Masayama <miyumasayama@Mac.lan>
2024-10-23 23:26:18 +0100

    feat/appbar

---
+++ src/app/components/appBar/appBar.tsx
@@ -0,0 +1,60 @@
+import {
+  AppBar as MuiAppBar,
+  Divider,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+} from '@mui/material';
+import { FC, } from 'react';
+
+const pages = ['About', 'Skills', 'Experience', 'Products'];
+
+export const AppBar: FC = () => {
+  return (
+    <MuiAppBar
+      elevation={0}
+      position={'fixed'}
+      sx={{
+        maxHeight: 60,
+        p: 0, 
+        ':after': { display: 'none' },
+      }}
+    >
+      <Toolbar
+        variant="dense"
+        sx={{
+          minHeight: 60,
+          backgroundColor: 'rgba(229, 171, 190, 0.5)',
+          fontFamily: 'monospace',
+        }}
+      >
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: 'monospace',
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Miyu's Portfolio
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+      </Toolbar>
+      <Divider />
+    </MuiAppBar>
+  );
+};
--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,10 +1,14 @@
+import { AppBar } from "@/app/components/appBar/appBar";
 import { Box } from "@mui/material";
 import { FC } from "react";
 
 export const Home: FC = () => {
     return (
+        <>
+        <AppBar />
         <Box>
             こんにちは
         </Box>
+        </>
     )
 }
