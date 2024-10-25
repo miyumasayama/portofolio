6cd1208e3410dd96bacbe65d7493ad362e2beb62
Miyu Masayama <miyumasayama@Mac.lan>
2024-10-25 14:09:58 +0100

    fix:不要なproperty削除

--- src/app/components/organisms/skills/fragments/skillSet.ts
+++ src/app/components/organisms/skills/fragments/skillSet.ts
@@ -2,72 +2,58 @@ export const skillSet = [
     {
         name: 'TypeScript',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/typescript.png`,
-        description: 'aaa'
     },
     {
         name: 'React',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/react.png`,
-        description: 'aaa'
     },
     {
         name: 'Redux',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/redux.png`,
-        description: 'aaa'
     },
     {
         name: 'RTKQuery',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/redux.png`,
-        description: 'aaa'
     },
     {
         name: 'Vue.js',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/vue.png`,
-        description: 'aaa'
     },
     {
         name: 'Next.js',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/next.png`,
-        description: 'aaa'
     },
     {
         name: 'MUI',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/mui.jpeg`,
-        description: 'aaa'
     },
     {
         name: 'Storybook',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/storybook.png`,
-        description: 'aaa'
     },
     {
         name: 'Docker',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/docker.png`,
-        description: 'aaa'
     },
     {
         name: 'Git',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/git.png`,
-        description: 'aaa'
     },
     {
         name: 'SQL',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/sql.png`,
-        description: 'aaa'
     },
     {
         name: 'Figma',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/figma.png`,
-        description: 'aaa'
     },
     {
         name: 'Laravel',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/laravel.png`,
-        description: 'aaa'
     },
     {
         name: 'Ruby on Rails',
         image: `${process.env.NEXT_PUBLIC_URL}//skills/rubyOnRails.png`,
-        description: 'aaa'
     },
 
 ]
