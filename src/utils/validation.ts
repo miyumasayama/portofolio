dabfa04d95cd58c63bfdabdde9c0cefdaac89cf6
Miyu Masayama <miyumasayama@Mac.lan>
2024-10-28 14:09:40 +0000

    feat/色の変更

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,3 +4,7 @@ export const total = 10;
 export const formatTimeToPad = (time: number) => {
   return String(time).padStart(2, "0");
 };
+
+export const isValid = (text: string, pattern: RegExp) => {
+  return new RegExp(pattern).test(text);
+};
