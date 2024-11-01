efa441d67910ab76ca51bc3259bc778691cafe4b
Miyu Masayama <miyumasayama@Miyus-Laptop.lan>
2024-11-01 12:31:01 +0000

    fix:サイマジョ追加

---
+++ src/components/pages/costume/data/sakuratsukiSeihuku3ki.tsx
@@ -0,0 +1,141 @@
+import { CSSProperties, FC } from "react";
+
+type Props = {
+  style?: CSSProperties;
+};
+
+export const SakuratsukiSeihuku3ki: FC<Props> = ({ style }) => {
+  return (
+    <svg
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
+      style={style}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M38 2.5H21L30.5 19L38 2.5Z"
+        fill="white"
+        stroke="white"
+        strokeWidth="0.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.5 60L10 40L13 28L45.5 27.5L49.5 39.5L53.5 60H7.5Z"
+        fill="white"
+        stroke="#EDEFEE"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M27.5 5.5L29 7L27.5 14L29 17.5L31.5 14L30 7.5L31.5 5.5L30 4L27.5 5.5Z"
+        fill="#33394F"
+        stroke="#33394F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21.5 2L25 0.5L30 3.5L33 0.5L37 2L33 6.5L30 3.5L25.5 6.5L21.5 2Z"
+        fill="white"
+      />
+      <path
+        d="M30 3.5L25 0.5L21.5 2L25.5 6.5L30 3.5ZM30 3.5L33 0.5L37 2L33 6.5L30 3.5Z"
+        stroke="#E0DFE3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21.5 2L6.5 3.5L0.5 33H6.5L12.5 14.5V27L10 39L25.5 44L29.5 27L34 44L50 39L46 27L47.5 14.5L52.5 33H59.5L52.5 3.5L38 2L29.5 18.5L21.5 2Z"
+        fill="#181A29"
+        stroke="#030305"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M29.5 18.5L32 23L29.5 27.5"
+        stroke="#090A0F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.5 3L18.5 11.5L15.5 14L31 22.5L42 14L40.5 11.5L46 3"
+        stroke="#090A0F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <circle cx="30" cy="24" r="1" fill="#D9D9D9" />
+      <path
+        d="M44 3L40 8.5"
+        stroke="#F2F3F2"
+        strokeWidth="0.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M43 3L39 8.5"
+        stroke="#F2F3F2"
+        strokeWidth="0.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M16 3L19 9.5"
+        stroke="#F2F3F2"
+        strokeWidth="0.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17 3L20 9.5"
+        stroke="#F2F3F2"
+        strokeWidth="0.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M16 42L14 59.5"
+        stroke="#F2F3F2"
+        strokeWidth="0.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M41.5 43L44 59"
+        stroke="#F2F3F2"
+        strokeWidth="0.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M57.5 24.5L51.5 29"
+        stroke="#F2F3F2"
+        strokeWidth="0.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2 26L8 29"
+        stroke="#F2F3F2"
+        strokeWidth="0.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M58 26.5L52 31"
+        stroke="#F2F3F2"
+        strokeWidth="0.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.5 28.5L7 31.5"
+        stroke="#F2F3F2"
+        strokeWidth="0.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
--- src/components/pages/costume/utils/costumes.ts
+++ src/components/pages/costume/utils/costumes.ts
@@ -1,6 +1,6 @@
 export const costumes = [
-  {
-    name: "夏の近道",
-  },
+  { name: "夏の近道" },
   { name: "流れ弾(制服)" },
+  { name: "桜月(制服)3期生" },
+  { name: "サイレントマジョリティ" },
 ];
